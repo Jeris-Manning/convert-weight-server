5c52f802cf4f0f298ca45ef476973cb98ceb1a25
Jeris Manning <the.goose.moose@gmail.com>
2020-01-23 23:29:28 -0700

    splitting backend into seperate repo

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const server = express();
 server.use(express.json());
 server.use(CORS());
 
+console.log("Server be runnin' boss");
+
 server.get('/api/conversions', async (req, res) => {
   try {
     const conversions = await Ingredients.get();
+    console.log(conversions);
     res.status(200).json(conversions);
   } catch (err) {
     res.status(500).json(err);
