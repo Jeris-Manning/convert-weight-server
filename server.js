98ca256e3a77e6d7b9fdc78e810d69f6a256a23e
Jeris Manning <the.goose.moose@gmail.com>
2020-01-24 17:21:20 -0700

    check check

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ console.log("Server be runnin' boss");
 server.get('/api/conversions', async (req, res) => {
   try {
     const conversions = await Ingredients.get();
-    console.log(conversions);
     res.status(200).json(conversions);
   } catch (err) {
     res.status(500).json(err);
